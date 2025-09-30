bc312901f7c49864f5fc2c0e72e8061e841c9d53
Andrew <andrew.shyika@gmail.com>
2025-09-30 10:21:42 +0300

    add 2_2

---
+++ c_labs/2_2.c
@@ -0,0 +1,26 @@
+// Введіть з клавіатури ціле число, якщо воно додатне,
+// перевірте, чи кратне 5, інакше – чи менше воно за -100.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int x;
+
+  puts("Input value:");
+  scanf("%f", &x);
+
+  if (x > 0)
+    if (x % 5 == 0)
+      printf("number %d multiple of 5\n", x);
+    else
+      printf("number %d is not multiple of 5\n\n", x);
+
+  if (x < -100)
+    printf("Value %.2f <=0 and <-100\n", x);
+  else
+    printf("Value %.2f <=0 and >=-100\n", x);
+
+  system("pause");
+  return 0;
+}
